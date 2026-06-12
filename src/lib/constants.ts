d0697dbc146afccd807e26a1bd8715976fbe4d64
Hiroshi Motegi <hiroshi.motegi@gmail.com>
2026-06-12 22:26:15 +0900

    chore: bump ONBOARDING_VERSION to 2 and TERMS_UPDATED_AT to trigger re-display

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 // Update this datetime (JST) whenever terms are revised.
 // All users who agreed before this time will be asked to re-agree.
-export const TERMS_UPDATED_AT = "2026-06-08T03:13:00+09:00";
+export const TERMS_UPDATED_AT = "2026-06-12T22:00:00+09:00";
 
 // Increment this when onboarding content changes and you want
 // all users (including existing) to see the new onboarding again.
-export const ONBOARDING_VERSION = 1;
+export const ONBOARDING_VERSION = 2;
